60c92c93e7988e168524f13a9ce28dd14a4802e0
Zachary Ebenfeld <zackebenfeld@gmail.com>
2025-11-17 19:56:02 -0500

    add symbol

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,22 @@
 import { Host, Text, VStack } from "@expo/ui/swift-ui";
+import { SymbolView } from "expo-symbols";
+import { StyleSheet } from "react-native";
 
 export default function ContentView() {
   return (
-    <Host style={{ flex: 1 }}>
+    <Host style={styles.container}>
       <VStack>
+        <SymbolView name="globe" />
         <Text>Hello, world!</Text>
       </VStack>
     </Host>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
